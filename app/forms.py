c2160a9341516d812ace40b55d62bf92bfc74012
makebiyaban <mak47f@gmail.com>
2024-01-20 17:32:15 +0330

    create more form and complate them

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from datetime import datetime
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,16 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign in')
+
+class SignUpForm(FlaskForm):
+    name = StringField('enter your name', validators=[DataRequired()])
+    lastname = StringField('enter your family name', validators=[DataRequired()])
+    email = StringField('enter your Email address', validators=[DataRequired()])
+    phone = StringField('enter your mobile phone number')
+    username = StringField('if you want choose usaername or ...', validators=[DataRequired()])
+    password = PasswordField('enter password', validators=[DataRequired()])
+    ##datetime = datetime()
+    submit = SubmitField('Sign Up')
+class LinkForm(FlaskForm):
+    link = StringField('link ra vared konid', validators=[DataRequired()])
+    submit = SubmitField('link ra kotah kon')
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,30 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import myapp
-from app.forms import LoginForm
+from app.forms import LoginForm, LinkForm, SignUpForm
 
-@myapp.route("/")
-@myapp.route("/index")
+@myapp.route("/", methods=['GET', 'POST'])
+@myapp.route("/index",  methods=['GET', 'POST'])
 def  index():
-	user={'name':'mohammad'}
-	return render_template('index.html',user=user)
+	form = LinkForm()
+	user = {'name':'mohammad'}
+	return render_template('index.html',user=user,form=form)
+
 
 
 @myapp.route('/login',  methods=['GET', 'POST'])
 def login():
-	form=LoginForm()
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('login requested for user {}, remember_me={}'.format(
+			form.username.data,form.remember_me.data))
+		return redirect(url_for('index'))
 	return render_template('login.html',form=form)
+
+
+
+@myapp.route('/signup',  methods=['GET', 'POST'])
+def signup():
+	form = SignUpForm()
+	return render_template('signup.html', form=form)
+
+
